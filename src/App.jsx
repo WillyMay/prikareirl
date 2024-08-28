86bf1392bc03e397614552f8f9e74dd5c8e3450a
WillyMay <willian.mayer@proton.me>
2024-08-28 08:46:15 -0400

    installing react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Nosotros from "./components/Nosotros";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hola! </h1>
+      <h1 className="text-3xl font-bold underline">Hola! </h1>
+      <Router basename="/prikareirl">
+        <Routes>
+          <Route path="/" element={<Home />}>
+          </Route>
+          <Route path="/nosotros" element={<Nosotros />}>
+          </Route>
+        </Routes>
+      </Router>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
