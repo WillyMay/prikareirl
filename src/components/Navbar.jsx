96ee9781e45fce935b8c40cee1da156cab052726
WillyMay <willian.mayer@proton.me>
2024-08-28 11:57:04 -0400

    arreglando boton home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ export default function Navbar(props) {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
-              <a href="/">
+              <a href="/prikareirl/#/">
                 {" "}
                 <img
                   alt="Your Company"
